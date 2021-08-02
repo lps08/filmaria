8cb746bc348934aedad2cbff0f0c94da4ce45b49
lps08 <lucianolps08@gmail.com>
2021-08-02 12:25:36 -0300

    create page favoritos

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -1,8 +1,47 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import './favoritos.css'
+import { toast } from 'react-toastify';
 
 export default function Favoritos() {
+    const [filmes, setFilmes] = useState([]);
+
+    useEffect(() => {
+        const minhaLista = localStorage.getItem('filmes');
+        setFilmes(JSON.parse(minhaLista) || []);
+    }, []);
+
+    function handleDelete(id) {
+        // filtra o array e devolve os items dada uma condição
+        let filtroFilmes = filmes.filter((filme) => {
+            return ( filme.id !== id )
+        })
+
+        setFilmes(filtroFilmes);
+        localStorage.setItem('filmes', JSON.stringify(filtroFilmes));
+        toast.success('Filme excluido com sucesso!')
+    }
+
     return (
-        <div>
-            fav
+        <div id="meus-filmes">
+            <h1>Meus filmes</h1>
+
+            {filmes.length === 0 && <span>Você não possui nenhum filme salvo.</span>}
+
+            <ul>
+                {filmes.map((filme) => {
+                    return (
+                        <li key={filme.id}>
+                            <span>{filme.nome}</span>
+
+                            <div>
+                                <Link to={`/filme/${filme.id}`}>Ver detalhes</Link>
+                                <button onClick={() => handleDelete(filme.id) }>Excluir</button>
+                            </div>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     );
 }
