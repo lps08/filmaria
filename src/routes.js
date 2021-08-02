0733b1438af4d919e967f366e8d2117f6d60a44e
lps08 <lucianolps08@gmail.com>
2021-08-02 09:48:43 -0300

    add favoritos route

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 
 import Home from './pages/Home';
 import Filme from './pages/Filme'
+import Favoritos from './pages/Favoritos';
 
 const Routes = () => {
     return (
@@ -12,6 +13,7 @@ const Routes = () => {
             <Switch>
                 <Route exact path="/" component={Home} />
                 <Route exact path="/filme/:id" component={Filme} />
+                <Route exact path="/favoritos/" component={Favoritos} />
             </Switch>
         </BrowserRouter>
     );
